c5eb2a4d3ff665f4fac0c8bea1b17eeb5480b1b6
Vladimir Kuznetsov <mistakster@gmail.com>
2020-10-14 14:54:50 +0500

    added mark button

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,25 @@
 import React, { memo } from 'react';
 import './List.css';
 
+const MarkButton = ({ item, onMark }) => (
+    <button
+        type="button"
+        className="list-item__button list-item__button_mark"
+        onClick={() => onMark(item)}
+    >
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            height="24"
+            width="24"
+            className="list-item__button-icon"
+        >
+            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2
+            15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
+        </svg>
+    </button>
+);
+
 const DeleteButton = ({ item, disabled, onDelete }) => (
     <button
         type="button"
@@ -21,7 +40,7 @@ const DeleteButton = ({ item, disabled, onDelete }) => (
     </button>
 )
 
-const Item = ({ item, isDeletePending, onDelete }) => {
+const Item = ({ item, isDeletePending, onDelete, onMark }) => {
     return (
         <div className="list-item">
             <span className="list-item__name">{item.name}</span>
@@ -30,6 +49,12 @@ const Item = ({ item, isDeletePending, onDelete }) => {
                     item={item}
                     disabled={isDeletePending}
                     onDelete={onDelete}
+                />
+            )}
+            {onMark && (
+                <MarkButton
+                    item={item}
+                    onMark={onMark}
                 />
             )}
         </div>
@@ -42,7 +67,7 @@ function isPending(statuses, id) {
     return !!(statuses && statuses[id] && statuses[id].pending);
 }
 
-const List = ({ items, status, deleteStatuses, onDelete }) => {
+const List = ({ items, status, deleteStatuses, onDelete, onMark }) => {
     if (status.succeeded) {
         if (items.length > 0) {
             return (
@@ -53,6 +78,7 @@ const List = ({ items, status, deleteStatuses, onDelete }) => {
                             item={item}
                             isDeletePending={isPending(deleteStatuses, item.id)}
                             onDelete={onDelete}
+                            onMark={onMark}
                         />
                     ))}
                 </div>
@@ -73,7 +99,7 @@ const List = ({ items, status, deleteStatuses, onDelete }) => {
     );
 };
 
-const ListWrapper = ({ title, items, status, deleteStatuses, onDelete }) => {
+const ListWrapper = ({ title, items, status, deleteStatuses, onDelete, onMark }) => {
     console.log(`refresh ${title}`);
 
     return (
@@ -84,6 +110,7 @@ const ListWrapper = ({ title, items, status, deleteStatuses, onDelete }) => {
                 status={status}
                 deleteStatuses={deleteStatuses}
                 onDelete={onDelete}
+                onMark={onMark}
             />
         </div>
     );
